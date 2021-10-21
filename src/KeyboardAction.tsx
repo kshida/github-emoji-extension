ce4ad452cebc4b6364ccbbc9ffea77c648d07aba
kshida <kohei.shida@flierinc.com>
2021-10-21 14:36:17 +0900

    adjust position of popup if it's out of browser

--- src/KeyboardAction.tsx
+++ src/KeyboardAction.tsx
@@ -66,10 +66,18 @@ const initPopupPosition = (textAreaElement: HTMLTextAreaElement) => {
   // Temporarily save the textarea in focus.
   setFocusArea(textAreaElement)
 
+  // Get the width of the browser.
+  const screenWidth = document.body.clientWidth;
+
+  // Calculate if a popup is out of the browser.
+  const isExceedsBrowserHeight = (cursorY - 340) <= 0;
+  const isExceedsBrowserWidth = (cursorX + 348) >= screenWidth;
+
   // Adjust the coordinates of the cursor so that it is at the bottom left of the popup.
   popup.style.display = 'block'
-  popup.style.top = `${cursorY - 340}px`
-  popup.style.left = `${cursorX}px`
+  // Adjust the position of the popup if it is out of the browser.
+  popup.style.top = isExceedsBrowserHeight ? `${cursorY + 25}px` : `${cursorY - 340}px`
+  popup.style.left = isExceedsBrowserWidth ? `${cursorX - 348}px` : `${cursorX}px`
 }
 
 /**
